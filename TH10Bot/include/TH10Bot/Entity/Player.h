11ab673b33ec42c84926ab9bcd8cd538dacd4a00
GiriMind <GiriMind@users.noreply.github.com>
2019-04-20 12:06:59 +0800

    优化内存读取

--- TH10Bot/include/TH10Bot/Entity/Player.h
+++ TH10Bot/include/TH10Bot/Entity/Player.h
@@ -39,22 +39,22 @@ namespace th
 		Player move(Direction dir) const;
 		Player moveSlow(Direction dir) const;
 
-		float_t powers;
-		int_t life;
-		// type == 0 Reimu
-		// type == 1 Marisa
-		int_t type;
-		// slow == 0 High Speed
-		// slow == 1 Low Speed
-		int_t slow;
-		float_t itemObtainRange;
-		// status == 0 重生状态，此时无敌
-		// status == 1 正常状态
-		// status == 2 死亡
-		// status == 3 Unknown
-		// status == 4 被弹、决死，此时放B可以决死
+		//float_t powers;
+		//int_t life;
+		//// type == 0 Reimu
+		//// type == 1 Marisa
+		//int_t type;
+		//// slow == 0 High Speed
+		//// slow == 1 Low Speed
+		//int_t slow;
+		//float_t itemObtainRange;
+		//// status == 0 重生状态，此时无敌
+		//// status == 1 正常状态
+		//// status == 2 死亡
+		//// status == 3 Unknown
+		//// status == 4 被弹、决死，此时放B可以决死
 		int_t status;
-		// 剩余无敌时间
-		int_t invinibleTime;
+		//// 剩余无敌时间
+		//int_t invinibleTime;
 	};
 }
