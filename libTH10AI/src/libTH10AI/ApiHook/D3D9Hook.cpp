b182901dcd8066b43194b45a30734814f411256e
GiriMind <GiriMind@users.noreply.github.com>
2019-09-08 07:29:32 +0800

    帧同步失败

--- libTH10AI/src/libTH10AI/ApiHook/D3D9Hook.cpp
+++ libTH10AI/src/libTH10AI/ApiHook/D3D9Hook.cpp
@@ -64,21 +64,14 @@ namespace th
 			THROW_DIRECTX_HRESULT(hr);
 
 		uint_t* vTable = (uint_t*)(*((uint_t*)device.p));
-		m_clear = HookFunc<Clear_t>(reinterpret_cast<LPVOID>(vTable[43]), &D3D9Hook::ClearHook);
-		m_beginScene = HookFunc<BeginScene_t>(reinterpret_cast<LPVOID>(vTable[41]), &D3D9Hook::BeginSceneHook);
-		m_endScene = HookFunc<EndScene_t>(reinterpret_cast<LPVOID>(vTable[42]), &D3D9Hook::EndSceneHook);
+		//m_beginScene = HookFunc<BeginScene_t>(reinterpret_cast<LPVOID>(vTable[41]), &D3D9Hook::BeginSceneHook);
+		//m_endScene = HookFunc<EndScene_t>(reinterpret_cast<LPVOID>(vTable[42]), &D3D9Hook::EndSceneHook);
+		//m_clear = HookFunc<Clear_t>(reinterpret_cast<LPVOID>(vTable[43]), &D3D9Hook::ClearHook);
 		m_present = HookFunc<Present_t>(reinterpret_cast<LPVOID>(vTable[17]), &D3D9Hook::PresentHook);
 	}
 
 	D3D9Hook::~D3D9Hook()
 	{
-	}
-
-	HRESULT D3D9Hook::ClearHook(IDirect3DDevice9* device, DWORD count, CONST D3DRECT* rects, DWORD flags,
-		D3DCOLOR color, float z, DWORD stencil)
-	{
-		D3D9Hook& d3d9Hook = D3D9Hook::GetInstance();
-		return d3d9Hook.clearHook(device, count, rects, flags, color, z, stencil);
 	}
 
 	HRESULT D3D9Hook::BeginSceneHook(IDirect3DDevice9* device)
@@ -93,6 +86,13 @@ namespace th
 		return d3d9Hook.endSceneHook(device);
 	}
 
+	HRESULT D3D9Hook::ClearHook(IDirect3DDevice9* device, DWORD count, CONST D3DRECT* rects, DWORD flags,
+		D3DCOLOR color, float z, DWORD stencil)
+	{
+		D3D9Hook& d3d9Hook = D3D9Hook::GetInstance();
+		return d3d9Hook.clearHook(device, count, rects, flags, color, z, stencil);
+	}
+
 	HRESULT D3D9Hook::PresentHook(IDirect3DDevice9* device, CONST RECT* sourceRect, CONST RECT* destRect,
 		HWND destWindowOverride, CONST RGNDATA* dirtyRegion)
 	{
@@ -100,35 +100,40 @@ namespace th
 		return d3d9Hook.presentHook(device, sourceRect, destRect, destWindowOverride, dirtyRegion);
 	}
 
-	HRESULT D3D9Hook::clearHook(IDirect3DDevice9* device, DWORD count, CONST D3DRECT* rects, DWORD flags,
-		D3DCOLOR color, float z, DWORD stencil)
-	{
-		m_clearTime = std::chrono::steady_clock::now();	// 帧开始时间？
-		return m_clear(device, count, rects, flags, color, z, stencil);
-	}
-
 	HRESULT D3D9Hook::beginSceneHook(IDirect3DDevice9* device)
 	{
-		m_beginSceneTime = std::chrono::steady_clock::now();	// 帧开始时间？
+		m_beginSceneTime = std::chrono::steady_clock::now();
 		return m_beginScene(device);
 	}
 
 	HRESULT D3D9Hook::endSceneHook(IDirect3DDevice9* device)
 	{
-		m_endSceneTime = std::chrono::steady_clock::now();	// 帧开始时间？
+		m_endSceneTime = std::chrono::steady_clock::now();
 		return m_endScene(device);
 	}
 
+	HRESULT D3D9Hook::clearHook(IDirect3DDevice9* device, DWORD count, CONST D3DRECT* rects, DWORD flags,
+		D3DCOLOR color, float z, DWORD stencil)
+	{
+		m_clearTime = std::chrono::steady_clock::now();
+		return m_clear(device, count, rects, flags, color, z, stencil);
+	}
+
+	// 帧同步失败
+	// 垂直同步的等待时间会在0-15之间递增或递减，或者长时间停留在0-1。
+	// 也就是说没时间读取数据和计算。
 	HRESULT D3D9Hook::presentHook(IDirect3DDevice9* device, CONST RECT* sourceRect, CONST RECT* destRect,
 		HWND destWindowOverride, CONST RGNDATA* dirtyRegion)
 	{
-		m_presentBeginTime = std::chrono::steady_clock::now();	// 帧结束时间
-		std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-			m_presentBeginTime - m_presentEndTime);	// 逻辑处理耗时
-		m_presentTimespan = 16 - interval.count();	// 垂直同步耗时
+		m_presentBeginTime = std::chrono::steady_clock::now();
 		notifyPresentBegin();
 		HRESULT hr = m_present(device, sourceRect, destRect, destWindowOverride, dirtyRegion);
-		m_presentEndTime = std::chrono::steady_clock::now();	// 帧开始时间
+		m_presentEndTime = std::chrono::steady_clock::now();
+		std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
+			m_presentEndTime - m_presentBeginTime);
+		m_presentTimespan = interval.count();
+		std::cout << m_presentTimespan << std::endl;
+		//BOOST_LOG_TRIVIAL(error) << m_presentTimespan;
 		//notifyPresentEnd();
 		return hr;
 	}
