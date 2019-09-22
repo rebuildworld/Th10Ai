822a35e0ea1a3db4e3726601f51cc84259b81e57
projMiss <22651937+projMiss@users.noreply.github.com>
2019-09-22 18:54:52 +0800

    emmm

--- libTH10Bot/src/libTH10Bot/FrameSync.cpp
+++ libTH10Bot/src/libTH10Bot/FrameSync.cpp
@@ -1,10 +1,6 @@
 #include "libTH10Bot/Common.h"
 #include "libTH10Bot/FrameSync.h"
 
-#include <thread>
-
-#include "libTH10Bot/Input.h"
-
 namespace th
 {
 	FrameSync::FrameSync() :
@@ -13,45 +9,15 @@ namespace th
 	{
 	}
 
-	std::chrono::steady_clock::time_point g_presentBeginTime;
-	std::chrono::steady_clock::time_point g_presentEndTime;
-	time_t g_presentTimespan;
-
 	void FrameSync::onPresentBegin(IDirect3DDevice9* device, CONST RECT* sourceRect, CONST RECT* destRect,
 		HWND destWindowOverride, CONST RGNDATA* dirtyRegion)
 	{
-		// 游戏逻辑的处理时间
-		g_presentBeginTime = std::chrono::steady_clock::now();
-		//std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-		//	g_presentBeginTime - g_getDeviceStateTime);
-		//std::cout << interval.count() << " ";
-
 		notifyPresent();
 	}
 
 	void FrameSync::onPresentEnd(HRESULT& hr, IDirect3DDevice9* device, CONST RECT* sourceRect, CONST RECT* destRect,
 		HWND destWindowOverride, CONST RGNDATA* dirtyRegion)
 	{
-		if (FAILED(hr))
-		{
-			std::cout << "Present()失败。" << std::endl;
-			return;
-		}
-
-		// 垂直同步的等待时间
-		g_presentEndTime = std::chrono::steady_clock::now();
-		std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-			g_presentEndTime - g_presentBeginTime);
-		//std::cout << interval.count() << std::endl;
-		g_presentTimespan = interval.count();
-
-		// 如果垂直同步的等待时间小于5毫秒，则延时一下，会导致掉帧
-		if (g_presentTimespan < 5)
-		{
-			time_t delayTimespan = 5 - g_presentTimespan;
-			std::cout << "作弊延时: " << delayTimespan << std::endl;
-			std::this_thread::sleep_for(std::chrono::milliseconds(delayTimespan));
-		}
 	}
 
 	void FrameSync::enable(bool enabled)
@@ -77,11 +43,6 @@ namespace th
 		}
 		m_presentReadied = false;
 		if (!waited)
-		{
-			std::chrono::steady_clock::time_point waitTime = std::chrono::steady_clock::now();
-			std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-				waitTime - g_presentBeginTime);
-			std::cout << "读取数据延迟了：" << interval.count() << std::endl;
-		}
+			std::cout << "读取掉帧。" << std::endl;
 	}
 }
--- libTH10Bot/src/libTH10Bot/Input.cpp
+++ libTH10Bot/src/libTH10Bot/Input.cpp
@@ -1,15 +1,10 @@
 #include "libTH10Bot/Common.h"
 #include "libTH10Bot/Input.h"
 
-#include <thread>
-
-#include "libTH10Bot/FrameSync.h"
-
 namespace th
 {
 	Input::Input() :
 		m_keyReadied(false),
-		m_keyDelayed(false),
 		m_di8Hook(this)
 	{
 		std::lock_guard<std::mutex> lock(m_keyMutex);
@@ -19,48 +14,12 @@ namespace th
 			keyState = KS_NONE;
 	}
 
-	std::chrono::steady_clock::time_point g_getDeviceStateTime;
-	time_t g_smoothTimespan;
-
 	void Input::onGetDeviceStateBegin(IDirectInputDevice8* device, DWORD size, LPVOID data)
 	{
-		// 帧平滑机制：获取输入前会有等待时间
-		g_getDeviceStateTime = std::chrono::steady_clock::now();
-		std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-			g_getDeviceStateTime - g_presentEndTime);
-		g_smoothTimespan = interval.count();
-		//std::cout << interval.count() << " ";
-
-		// c_dfDIKeyboard
-		if (size == 256 && data != nullptr)
-		{
-			//// 如果前一帧的垂直同步等待时间 + 当前帧的平滑等待时间 < 5毫秒，则延时一下
-			//if (g_presentTimespan + g_smoothTimespan < 5)
-			//{
-			//	time_t delayTimespan = 5 - g_presentTimespan - g_smoothTimespan;
-			//	std::cout << "作弊延时: " << delayTimespan << std::endl;
-			//	std::this_thread::sleep_for(std::chrono::milliseconds(delayTimespan));
-			//	g_getDeviceStateTime = std::chrono::steady_clock::now();
-			//}
-		}
-		// c_dfDIMouse
-		//sizeof(DIMOUSESTATE);
-		// c_dfDIMouse2
-		//sizeof(DIMOUSESTATE2);
-		// c_dfDIJoystick
-		//sizeof(DIJOYSTATE);
-		// c_dfDIJoystick2
-		//sizeof(DIJOYSTATE2);
 	}
 
 	void Input::onGetDeviceStateEnd(HRESULT& hr, IDirectInputDevice8* device, DWORD size, LPVOID data)
 	{
-		if (FAILED(hr))
-		{
-			std::cout << "GetDeviceState()失败。" << std::endl;
-			return;
-		}
-
 		// c_dfDIKeyboard
 		if (size == 256 && data != nullptr)
 		{
@@ -81,11 +40,10 @@ namespace th
 					}
 				}
 				m_keyReadied = false;
-				m_keyDelayed = false;
 			}
 			else
 			{
-				m_keyDelayed = true;
+				std::cout << "输入掉帧1。" << std::endl;
 			}
 		}
 		// c_dfDIMouse
@@ -133,15 +91,8 @@ namespace th
 	{
 		std::lock_guard<std::mutex> lock(m_keyMutex);
 		memcpy_s(m_readState, sizeof(m_readState), m_writeState, sizeof(m_writeState));
+		if (m_keyReadied)
+			std::cout << "输入掉帧2。" << std::endl;
 		m_keyReadied = true;
-		if (m_keyDelayed)
-		{
-			std::chrono::steady_clock::time_point inputTime = std::chrono::steady_clock::now();
-			std::chrono::milliseconds interval = std::chrono::duration_cast<std::chrono::milliseconds>(
-				inputTime - g_presentBeginTime);
-			std::cout << "输入延迟了：" << g_presentTimespan + g_smoothTimespan
-				<< " " << interval.count() << std::endl;
-			m_keyDelayed = false;
-		}
 	}
 }
